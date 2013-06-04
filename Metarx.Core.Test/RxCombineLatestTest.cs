c0e93da4c33b858a0fb755afde3bcc36f10229e7
eih <einarwh@gmail.com>
2013-06-04 16:14:41 +0200

    threading macro test

--- Metarx.Core.Test/RxCombineLatestTest.cs
+++ Metarx.Core.Test/RxCombineLatestTest.cs
@@ -48,5 +48,29 @@ namespace Metarx.Core.Test
             Assert.AreEqual(3, results.Count());
             Assert.AreEqual("Found a face at the right height", results.First());
         }
+
+        [TestMethod]
+        public void WorksWithFacesAndHeightsThreadingMacro()
+        {
+            var values = new[]
+                {
+                    new Tuple<string, string>("navdata", "[{ \"fooness\": \"superfoo\", \"altitude\": -0.239784 }]"),
+                    new Tuple<string, string>("navdata", "[{ \"fooness\": \"moarfoo\", \"altitude\": 0.573957 }]"),
+                    new Tuple<string, string>("faces", "[{ \"barity\": \"bar\", \"confidence\": 0.42347 }]"),
+                    new Tuple<string, string>("navdata", "[{ \"fooness\": \"superfoo\", \"altitude\": 1.687834 }]"),
+                    new Tuple<string, string>("faces", "[{ \"barity\": \"bar\", \"confidence\": 0.94728 }]"),
+                    new Tuple<string, string>("navdata", "[{ \"fooness\": \"superfoo\", \"altitude\": 1.687834 }]"),
+                    new Tuple<string, string>("navdata", "[{ \"fooness\": \"moarfoo\", \"altitude\": 2.5493784 }]"),
+                    new Tuple<string, string>("faces", "[{ \"barity\": \"bar\", \"confidence\": 1.8381293 }]"),
+                    new Tuple<string, string>("navdata", "[{ \"fooness\": \"fooest\", \"altitude\": 3.5634756 }]"),
+                    new Tuple<string, string>("navdata", "[{ \"fooness\": \"fooest\", \"altitude\": 4.384927 }]"),
+                };
+
+            const string SingleQuoteProgram = "(define (execute stream) (rx-combine-latest (lambda (c h) 'Found a face at the right height') (->> stream (rx-where (lambda (t) (= 'faces' ((method get_Item1) t)))) (rx-select (method get_Item2)) (rx-where (lambda (t) (> ((method get_Length) t) 4))) (rx-select (lambda (s) (jdv-parse 'confidence' (invoke-instance s 'Substring' 2 (- ((method get_Length) s) 4))))) (rx-where (lambda (c) (> c 1.0)))) (->> stream (rx-where (lambda (t) (= 'navdata' ((method get_Item1) t)))) (rx-select (method get_Item2)) (rx-select (lambda (s) (jdv-parse 'altitude' (invoke-instance s 'Substring' 2 (- ((method get_Length) s) 4))))) (rx-where (lambda (d) (and (not (invoke-static 'System.Double' 'IsNaN' d)) (> d 0.2)))))))";
+            var program = SingleQuoteProgram.Replace('\'', '"');
+            var results = Execute(program, values).ToList();
+            Assert.AreEqual(3, results.Count());
+            Assert.AreEqual("Found a face at the right height", results.First());
+        }
     }
 }
